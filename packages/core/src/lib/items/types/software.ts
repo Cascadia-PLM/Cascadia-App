d6152edc9890a65905ada800922350d80193d405
Artur Klujewski <artur.klujewski@gmail.com>
2026-08-31 10:33:19 +0200

    lint fix

--- packages/core/src/lib/items/types/software.ts
+++ packages/core/src/lib/items/types/software.ts
@@ -2,8 +2,8 @@
 // Copyright (c) 2026 Cascadia PLM LLC
 
 import { z } from 'zod'
-import type { RefinementCtx } from 'zod'
 import { baseItemSchema, commonStates } from './base'
+import type { RefinementCtx } from 'zod'
 import type { BaseItem, RelationshipConfig } from './base'
 
 // ============================================================================
